53fb1d0d7af237d8921b7a102d0219fdc050a42b
maxFritzen <max.fritzen@hotmail.se>
2018-04-21 17:20:14 +0200

    Fixed scroll bug for IE. added babel, uglify. Cleaned up files.

---
+++ scripts/index.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var Scroll = function Scroll(id) {
+  // Ta in ID att scrolla till.
+  // Hitta det ID:t
+  var targetId = document.getElementById(id);
+
+  // event.preventDefault();
+
+  // Är jag inte vid domRect.y, fortsätt scrolla en pixel eller fler(scrollLength).
+  var targetPos = targetId.getBoundingClientRect().top;
+  // console.log(targetId.getBoundingClientRect());
+  var currentPos = 0;
+
+  // Kolla ifall targetPos är negativt eller positivt
+  var direction = targetPos > 0; // Borde också kolla ifall targetPos  === 0
+
+
+  if (direction) {
+    // true, means scroll down
+    var scrollLength = 45;
+    var intr = setInterval(function () {
+      //
+
+
+      window.scrollBy(0, scrollLength);
+      currentPos += scrollLength;
+      //Framme ? stanna där
+      if (currentPos + 20 > targetPos) {
+        clearInterval(intr);
+      }
+    }, 10);
+  } else {
+    // Scroll up
+    var _scrollLength = -30;
+    var _intr = setInterval(function () {
+      window.scrollBy(0, _scrollLength);
+      currentPos += _scrollLength;
+      if (currentPos < targetPos) {
+        clearInterval(_intr);
+      }
+    }, 10);
+  }
+};
--- scripts/smooth-scroll.js
+++ scripts/smooth-scroll.js
@@ -1,26 +1,34 @@
 const Scroll = (id) => {
   // Ta in ID att scrolla till.
   // Hitta det ID:t
-  const targetId = document.getElementById(id);
+  var targetId = document.getElementById(id);
+
   // event.preventDefault();
 
   // Är jag inte vid domRect.y, fortsätt scrolla en pixel eller fler(scrollLength).
-    const targetPos = targetId.getBoundingClientRect().y;
+    var targetPos = targetId.getBoundingClientRect().top;
+    // console.log(targetId.getBoundingClientRect());
     let currentPos = 0;
 
     // Kolla ifall targetPos är negativt eller positivt
-    const direction = targetPos > 0; // Borde också kolla ifall targetPos  === 0
+    var direction = targetPos > 0; // Borde också kolla ifall targetPos  === 0
+
+
     if ( direction ) { // true, means scroll down
-      const scrollLength = 30;
+      const scrollLength = 45;
       const intr = setInterval(() => {
+        //
+
 
         window.scrollBy(0, scrollLength);
         currentPos += scrollLength;
         //Framme ? stanna där
-        if ( currentPos + 20 > targetPos) { clearInterval(intr) }
+        if ( currentPos + 20 > targetPos) {
+          clearInterval(intr);
+         }
       }, 10);
     } else { // Scroll up
-      const scrollLength = -20;
+      const scrollLength = -30;
       const intr = setInterval(() => {
         window.scrollBy(0, scrollLength);
         currentPos += scrollLength;
