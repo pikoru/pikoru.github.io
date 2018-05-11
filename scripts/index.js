0ce763179a3c91995610bfd762af60a2a22ad24b
maxFritzen <max.fritzen@hotmail.se>
2018-05-11 22:18:01 +0200

    Forgot to run build-script

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,18 @@
+'use strict';
+
+var Submit = function Submit() {
+  alert('submit');
+};
+
+var Click = function Click() {
+  console.log('click');
+};
 "use strict";
 
-console.log('functions are loaded correctly');
-var config = {
-  apiKey: "AIzaSyDFWC9p9viBaemsqO3WLDCPE2tjCH65Ey4",
-  authDomain: "pikoru-afa35.firebaseapp.com",
-  databaseURL: "https://pikoru-afa35.firebaseio.com",
-  projectId: "pikoru-afa35",
-  storageBucket: "pikoru-afa35.appspot.com",
-  messagingSenderId: "361342662087"
-};
-firebase.initializeApp(config);
-var database = firebase.database();
-var Scroll = function Scroll(id) {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var Scroll = exports.Scroll = function Scroll(id) {
   // Ta in ID att scrolla till.
   // Hitta det ID:t
   var targetId = document.getElementById(id);
@@ -51,63 +52,5 @@ var Scroll = function Scroll(id) {
         clearInterval(_intr);
       }
     }, 10);
-  }
-};
-
-// const OnSubmit = (e) => {
-//   e.preventDefault();
-//   console.log('OnSubmit called');
-//   const email = document.getElementById('email-input').value;
-//   console.log(email);
-//
-//   return database.ref('emails').push(email)
-//   .then(() => {
-//     alert('Email added!!!');
-//   }).catch((e) => {
-//     console.log('error:', e.message);
-//   });
-//
-// }
-
-// function validateEmail(email) {
-//     var char = '@';
-//     return char.test(String(email).toLowerCase());
-// }
-
-var onSubmit = function onSubmit(e) {
-  e.preventDefault();
-  var email = document.getElementById('email-input').value;
-  validateEmail(email);
-};
-
-var validateEmail = function validateEmail(email) {
-  console.log('validate', email);
-  var t = /@/;
-  var res = t.test(email);
-  console.log(res);
-  if (email.length > 2 && res) {
-    return database.ref('emails').push(email).then(function () {
-      showAlert('success');
-    }).catch(function (e) {
-      console.log('error:', e.message);
-      showAlert('error');
-    });
-  } else {
-    console.log('error with email');
-    showAlert('error');
-  }
-};
-
-var showAlert = function showAlert(type) {
-  switch (type) {
-    case 'success':
-      document.getElementById('success-text').style.display = 'block';
-      document.getElementById('error-text').style.display = 'none';
-      break;
-    case 'error':
-      console.log('error');
-      document.getElementById('error-text').style.display = 'block';
-      document.getElementById('success-text').style.display = 'none';
-      break;
   }
 };
